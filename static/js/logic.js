634234e7f18475e1f0244d0c1d22e621ae51f94d
Yuting Gao <yg2790@columbia.edu>
2022-05-22 19:45:43 +0800

    update.

--- static/js/logic.js
+++ static/js/logic.js
@@ -9,7 +9,7 @@ console.log('working');
 // });
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}',{
+let dayNav = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-day-v1/tiles/{z}/{x}/{y}?access_token={accessToken}',{
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -17,7 +17,7 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
 
 
 // We create the dark view tile layer that will be an option for our map.
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let nightNav = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-night-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -25,22 +25,22 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-    Street: streets,
-    Dark: dark
+    Day: dayNav,
+    Night: nightNav
   };
 
   // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-    center: [30, 30],
+    center: [44.0, -80.0],
     zoom: 2,
-    layers: [streets]
+    layers: [dayNav]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
 // Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+dayNav.addTo(map);
 
 // // Add marker to map for Los Angeles California
 // // var marker = L.marker([34.0522, -118.2437]).addTo(map);
@@ -133,16 +133,37 @@ streets.addTo(map);
 // //      }
 // // });
 
-// Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/yg2790/Mapping_Earthquakes/main/majorAirports.json";
+// // Accessing the airport GeoJSON URL
+// let airportData = "https://raw.githubusercontent.com/yg2790/Mapping_Earthquakes/main/majorAirports.json";
+
+// // Grabbing our GeoJSON data.
+// d3.json(airportData).then(function(data){
+//     // Creating a GeoJSON layer with the retrieved data.
+//     L.geoJSON(data, {
+//         onEachFeature: function(feature, layer) {
+//             console.log(layer);
+//             layer.bindPopup("<h2>Airport Code: " + feature.properties.faa + "</h2> <hr> <h3>Aiport name: " + feature.properties.name + "</h3>") 
+//        }
+//     }).addTo(map)
+// });
+
+// Accessing the Toronto airline routes GeoJSON URL.
+let torontoData = "https://raw.githubusercontent.com/yg2790/Mapping_Earthquakes/main/torontoRoutes.json";
+
+// Create a style for the lines.
+let myStyle = {
+    color: "#ffffa1",
+    weight: 2
+}
 
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data){
-    // Creating a GeoJSON layer with the retrieved data.
-    L.geoJSON(data, {
-        onEachFeature: function(feature, layer) {
+d3.json(torontoData).then(function(data){
+    console.log(data);
+    L.geoJSON(data,{
+        style: myStyle,
+        onEachFeature: function(feature,layer) {
             console.log(layer);
-            layer.bindPopup("<h2>Airport Code: " + feature.properties.faa + "</h2> <hr> <h3>Aiport name: " + feature.properties.name + "</h3>") 
-       }
+            layer.bindPopup("<h2>Airline: " + feature.properties.airline + "</h2> <hr> <h3>Destination: "+ feature.properties.dst + "</h3>")
+    }
     }).addTo(map)
 });
