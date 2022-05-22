fb1cf17d860817857df1e98977baaa90a4677460
Yuting Gao <yg2790@columbia.edu>
2022-05-22 19:57:53 +0800

    update.

--- static/js/logic.js
+++ static/js/logic.js
@@ -9,7 +9,7 @@ console.log('working');
 // });
 
 // We create the tile layer that will be the background of our map.
-let dayNav = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-day-v1/tiles/{z}/{x}/{y}?access_token={accessToken}',{
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}',{
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -17,7 +17,7 @@ let dayNav = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-day
 
 
 // We create the dark view tile layer that will be an option for our map.
-let nightNav = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-night-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -25,22 +25,22 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-    Day: dayNav,
-    Night: nightNav
+    'Streets': streets,
+    'Satellite Streets': satelliteStreets
   };
 
   // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-    center: [44.0, -80.0],
-    zoom: 2,
-    layers: [dayNav]
+    center: [43.7, -79.3],
+    zoom: 11,
+    layers: [streets]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-// Then we add our 'graymap' tile layer to the map.
-dayNav.addTo(map);
+// // Then we add our 'graymap' tile layer to the map.
+// satelliteStreets.addTo(map);
 
 // // Add marker to map for Los Angeles California
 // // var marker = L.marker([34.0522, -118.2437]).addTo(map);
@@ -147,23 +147,40 @@ dayNav.addTo(map);
 //     }).addTo(map)
 // });
 
-// Accessing the Toronto airline routes GeoJSON URL.
-let torontoData = "https://raw.githubusercontent.com/yg2790/Mapping_Earthquakes/main/torontoRoutes.json";
+// // Accessing the Toronto airline routes GeoJSON URL.
+// let torontoData = "https://raw.githubusercontent.com/yg2790/Mapping_Earthquakes/main/torontoRoutes.json";
 
-// Create a style for the lines.
-let myStyle = {
-    color: "#ffffa1",
-    weight: 2
-}
+// // Create a style for the lines.
+// let myStyle = {
+//     color: "#ffffa1",
+//     weight: 2
+// }
+
+// // Grabbing our GeoJSON data.
+// d3.json(torontoData).then(function(data){
+//     console.log(data);
+//     L.geoJSON(data,{
+//         style: myStyle,
+//         onEachFeature: function(feature,layer) {
+//             console.log(layer);
+//             layer.bindPopup("<h2>Airline: " + feature.properties.airline + "</h2> <hr> <h3>Destination: "+ feature.properties.dst + "</h3>")
+//     }
+//     }).addTo(map)
+// });
+
+// Accessing the Toronto neighborhoods GeoJSON URL.
+let torontoHoods = "https://raw.githubusercontent.com/yg2790/Mapping_Earthquakes/main/torontoNeighborhoods.json"
 
 // Grabbing our GeoJSON data.
-d3.json(torontoData).then(function(data){
+d3.json(torontoHoods).then(function(data){
     console.log(data);
     L.geoJSON(data,{
-        style: myStyle,
-        onEachFeature: function(feature,layer) {
+        color: 'blue',
+        weight: 1,
+        fillColor: '#ffffa1',
+        onEachFeature: function(feature, layer) {
             console.log(layer);
-            layer.bindPopup("<h2>Airline: " + feature.properties.airline + "</h2> <hr> <h3>Destination: "+ feature.properties.dst + "</h3>")
-    }
+            layer.bindPopup("<h2>Neighborhood: " + feature.properties.AREA_NAME + "</h2>")
+        }
     }).addTo(map)
-});
+})
